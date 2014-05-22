4c933d0a5ecf6e783d2ed94352f5ff50d077d177
husni75 <multistation2@gmail.com>
2014-05-22 15:46:40 +0700

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -1019,28 +1019,29 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16, #??
         DUST_THRESHOLD=0.03e8, #??
     ),
-    hawaiicoin=math.Object(
-        P2P_PREFIX='7c1f9184'.decode('hex'),
-        P2P_PORT=9335,
-        ADDRESS_VERSION=0,
-        RPC_PORT=9334,
+    indocoin=math.Object(
+        P2P_PREFIX='c0c0c0c0'.decode('hex'),
+        P2P_PORT=19457,
+        ADDRESS_VERSION=38,
+        RPC_PORT=19451,
         RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'hawaiicoinaddress' in (yield bitcoind.rpc_help()) and
+            'indocoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         )),
-        SUBSIDY_FUNC=lambda height: 500*100000000 >> (height + 1)//500000,
+        SUBSIDY_FUNC=lambda height: 20000*19450000000  >> (height + 1)//500000,
         POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
-        BLOCK_PERIOD=50, # s
-        SYMBOL='HIC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Hawaiicoin') if platform.system() == 'Windows' 
-				else os.path.expanduser('~/Library/Application Support/Hawaiicoin/') if platform.system() == 'Darwin' 
-				else os.path.expanduser('~/.hawaiicoin'), 'hawaiicoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://pool.privanon.com:8080/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://pool.privanon.com:8080/address/',
-        TX_EXPLORER_URL_PREFIX='http://pool.privanon.com:8080/tx/',
+        BLOCK_PERIOD=60, # s
+        SYMBOL='IDC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'indocoin') if platform.system() == 'Windows'
+                                else os.path.expanduser('~/Library/Application Support/indocoin/') if platform.system() == 'Darwin'
+                                else os.path.expanduser('~/.indocoin'), 'indocoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://indocoin.info/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://indocoin.info/address/',
+        TX_EXPLORER_URL_PREFIX='http://indocoin.info/tx/',
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
+
     ),
 
 )
